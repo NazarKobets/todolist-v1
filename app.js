efbef9ada6c8c3d38f84ccbd715a79e05241770a
NazarKobets <66803921+NazarKobets@users.noreply.github.com>
2022-11-29 22:58:38 +0200

    add style.css

--- app.js
+++ app.js
@@ -3,41 +3,33 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     let today = new Date();
-    let currentDay = today.getDay();
-    let day = '';
 
-    switch (currentDay) {
-        case 0:
-        day = 'Sunday'
-            break;
-        case 1:
-        day = 'Monday'
-            break;
-        case 2:
-        day = 'Tuesday'
-            break;
-        case 3:
-        day = 'Wednesday'
-            break;
-        case 4:
-        day = 'Thursday'
-            break;
-        case 5:
-        day = 'Friday'
-            break;
-        case 6:
-        day = 'Saturday'
-            break;
-        default:
-        console.log('Error: current day is equal to: ' + currentDay);
-        break;
-    }
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
 
-    res.render('list', {day: day})
+    let day = today.toLocaleDateString('en-US', options);
+
+    res.render('list', {day: day, newListItems: items});
+});
+
+app.post('/', (req, res) => {
+    let item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
